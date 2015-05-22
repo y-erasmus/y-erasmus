609b7eaf11d28a722e66a68d89e969fdca380269
Thomas Burgio <thomas.burgio@y-nov.com>
2015-05-22 18:47:00 +0200

    modif thomas

--- Content/list.php
+++ Content/list.php
@@ -1,13 +1,41 @@
-
 <section class="users-list">
+  <h1 class="title-XL">Découvrir des profils !</h1>
+
+    <div class="outer-select half-left">
+      <form action="<?php echo URL ?>" autocomplete="off" method="post" class="xhrForm">
+      <input type="hidden" name="todo" value="filterbylang">
+      <select name="Sort" class="form-control textbox inputtext">
+        <option selected disabled>Trier par langue</option>
+        <option value="1">Français</option>
+        <option value="2">English</option>
+        <option value="3">Español</option>
+        <option value="4">русский</option>
+          <option value="5">العربية</option>
+          <option value="6">中国</option>
+          <option value="7">Deutsch</option>
+          <option value="8">日本人</option>
+          <option value="9">Português</option>
+          <option value="10">हिन्दी</option>
+        </select>
+    </div>
+
+      <button type="submit" class="btn btn-default TriLangueButton subbtn">Rechercher</button>
+  </form>
+
+  <?php
+  foreach($users as $user){ ?>
     <div class="user">
         <div class="left">
             <img src="Assets/images/img2.png" alt=""/>
         </div>
         <div class="right">
-            <p>Pseudo</p>
-            <p>nom</p>
-            <p>prénom</p>
+            <p> Pseudo : <?php echo $user['Pseudo'] ?> </p>
+            <p> Nom : <?php echo $user['Nom'] ?>    </p>
+            <p> Prénom : <?php echo $user['Prenom'] ?> </p>
         </div>
     </div>
+
+<?php
+}
+?>
 </section>
--- Controllers/listController.php
+++ Controllers/listController.php
@@ -1,55 +1,23 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Carlos
- * Date: 22/05/2015
- * Time: 14:56
- */
-
->>>>>>> origin/charlse
 /*======== A J A X =========*/
 $todo = coreFormat::sanitizePost('todo');
 switch($todo){
-    case 'connection':
+    case 'filterbylang':
+      echo $_POST['Sort']
 
     break;
-
 }
 
-/*=======R E D I R E C T ========*/
+$query = CoreSql::getPDO()->prepare("SELECT Pseudo, Nom, Prenom FROM user;");
+$go = $query->execute();
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
 
+
+
+/*=======R E D I R E C T ========*/
 
 require_once '/include/header.php';
 require_once '/content/list.php';
 require_once '/include/footer.php';
-/*
-if(isset($_SESSION['mail'], $_SESSION['fingerprint'])){
-    $query = CoreSql::getPDO()->prepare("SELECT fingerprint FROM user_admin WHERE mail = :mail;");
-    $query->bindParam(':mail', $_SESSION['mail'] , PDO::PARAM_STR);
-    $go = $query->execute();
-    $user = $query->fetch(PDO::FETCH_ASSOC);
-    if($user){
-        if($user['fingerprint'] == $_SESSION['fingerprint']){
-            require_once '/include/header.php';
-            require_once '/include/adminMenu.php';
-            echo CoreContent::getAdminResultsHtml();
-            require_once '/include/footer.php';
-        }else{
-            require_once '/include/header.php';
-            require_once '/content/loginForm.php';
-            require_once '/include/footer.php';
-        }
-    }else{
-        require_once '/include/header.php';
-        require_once '/content/loginForm.php';
-        require_once '/include/footer.php';
-    }
-}else{
-    require_once '/include/header.php';
-    require_once '/content/loginForm.php';
-    require_once '/include/footer.php';
-}*/
-
 
 ?>
